55958d0701b5d7738adac08d216c2165ac33b6c2
Robert Ignasiak <tori@robert-i.com>
2014-05-24 00:42:05 +0200

    Added database connect

---
+++ app/model.rb
@@ -0,0 +1,35 @@
+require 'pg'
+
+class PostgresConnector
+	def connect 
+	    db_parts = ENV['DATABASE_URL'].split(/\/|:|@/)
+	    username = db_parts[3]
+	    password = db_parts[4]
+	    host = db_parts[5]
+	    db = db_parts[7]
+	    @conn = PGconn.open(:host =>  host, :dbname => db, :user=> username, :password=> password)
+	end
+
+	# Create tables
+	def createTables
+		@conn.exec("DROP TABLE IF EXISTS query CASCADE;
+		CREATE TABLE query (
+			id_query serial PRIMARY KEY,
+			query_text text NOT NULL,
+			date timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+		);
+		");
+	end
+
+	def disconnect
+    	@conn.close
+  	end
+end
+
+
+def createModel
+	p = PostgresConnector.new()
+	p.connect
+	p.createTables
+	p.disconnect
+end
--- web.rb
+++ web.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require './app/model'
 
 get '/' do
 	erb :index
+end
+
+get '/reset' do
+	createModel
 end
