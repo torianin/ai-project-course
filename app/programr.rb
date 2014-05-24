ff17fa01e4cdcbebc285ade54021d553d49e182d
Robert Ignasiak <tori@robert-i.com>
2014-05-24 04:19:37 +0200

    Debug

--- app/programr.rb
+++ app/programr.rb
@@ -9,7 +9,7 @@ class Alice
 
 	def askAlice(question)
 		removeSpacialChars(question)
-		reaction = @robot.get_reaction(question)
+		reaction = @robot.get_reaction(question) + " " + question
 	end
 end
 
