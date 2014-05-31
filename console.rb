a0622fce42ee8c9cf40579276555cab5add6ab8e
Robert Ignasiak <tori@robert-i.com>
2014-05-31 22:33:27 +0200

    Working pusher

--- console.rb
+++ console.rb
@@ -3,5 +3,5 @@ require 'pusher'
 Pusher.url = "http://0b6500a2c511ef6a91ba:81572065aa966eb9805d@api.pusherapp.com/apps/76635"
 
 Pusher['test_channel'].trigger('my_event', {
-  message: 'hello world'
+  message: 'hello world\r\n'
 })
