5ecdbfbfde6fe6cc8daf70b30228fa5310961d3c
Robert Ignasiak <tori@robert-i.com>
2014-06-07 06:05:48 +0200

    Added simple calc

--- app/extra_questions.rb
+++ app/extra_questions.rb
@@ -11,9 +11,10 @@ def checkExtra(question)
     t = t + 2*3600
     t = t.strftime("%d/%m/%Y")
     "Dzisiaj jest #{t} :p"
+  elsif question =~ /ile/
+    q = question.delete('^+-*/01234567890()%')
+    return eval(q)
   else
     return true
   end
 end
-#message = gets.chomp
-#puts checkExtra(message)
