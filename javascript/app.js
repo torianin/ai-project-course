77485f59f6668663df2e98dcc0de8aefba7f0b02
Robert Ignasiak <tori@robert-i.com>
2014-06-07 03:13:37 +0200

    Dodanie wyboru trybu

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
         var pusher = new Pusher('0b6500a2c511ef6a91ba');
         
         var channel = pusher.subscribe('test_channel');
-        channel.bind('my_event', function(data) {
+        channel.bind(session, function(data) {
             var term = $('#term').terminal();
             term.echo(data.message);
         });
