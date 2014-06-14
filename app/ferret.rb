80185703c8f51f7ae5d8a55891d5cf865535a0be
Robert Ignasiak <tori@robert-i.com>
2014-06-14 17:36:57 +0200

    Test

--- app/ferret.rb
+++ app/ferret.rb
@@ -28,17 +28,14 @@ class Robert
     @index.search_each("q:#{question}") do | id, score |  
       if @index[id][:a]!=nil
         return "#{@index[id][:a]}" 
-      # else
-      #   regexp_value =  /#{@index[id][:q1]}/.match(q)[1]
-      #   a = @index[id][:r].gsub(/[*]/, regexp_value)
-      #   return "#{a}" 
+      else
+        regexp_value =  /#{@index[id][:q1]}/.match(q)[1]
+        a = @index[id][:r].gsub(/[*]/, regexp_value)
+        return "#{a}" 
       end
     end
-    return_messages = Array.new()
-    return_messages[0] = "Chyba coś źle napisałeś, nie chodziło ci przypadkiem o słowo #{@correction} :p"
-    return_messages[1] = "Wydaje mi się, że miałeś na myśli słowo #{@correction} :p"
-    return_messages[2] = "A nie powinno być #{@correction} zamiast #{word} :p"
-    return return_messages[Random.rand(3)]
+      return_messages = ["Nie kumam :p", "Nie mam pojecia jak Ci odpowiedziec :p", "Możemy zmienić temat :p", "Co robisz jutro o 18 :p"]
+      return return_messages[Random.rand(4)]
   end
 end
 
@@ -61,3 +58,11 @@ index.search_each('q1|q2|q3|q4:kocham cie') do | id, score |
     end
 end
 =end
+
+#OK <srai> - pozwala definiować synonimy dla pewnych określonych wyrażeń, 
+#OK <random> - pozwala wybrać programowi losowy element z listy możliwych; elementy te definiuje się pomiędzy znacznikami <li> i </li>,
+#OK <that> - pozwala sprawdzić jedną z poprzednią wypowiedź chatterbota, używane do łączenia odpowiedzi w zależności od ostatniego zdania, // działa
+# <think> - używane do przeprowadzenia czynności, które nie mają być wyświetlone dla użytkownika (np. zmiana wartości zmiennej),
+# < get/ > i < set > służą do zapisywania i odczytywania zmiennych,
+# <condition> - określa określać reakcję w zależności od stanu zmiennej,
+# <topic> - pozwala botowi zawęzić pole wyszukiwań szablonów do zadanego działu.
