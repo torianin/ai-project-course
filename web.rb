12f04f65bdae08c7583d2dfdee69f24ba761ea81
Robert Ignasiak <tori@robert-i.com>
2014-06-14 12:00:52 +0200

    Pytanie o fix

--- web.rb
+++ web.rb
@@ -56,6 +56,7 @@ class Public < Sinatra::Base
 		if session[:name] == nil
 			if checkName(params[:query])
 				session[:name] = params[:query]
+				return "Witaj #{session[:name]} :p"
 			else 
 				return "Albo rodzice nazwali Cię mega śmiesznie, albo nie ma takiego imienia i próbujesz mnie oszukać :p"
 			end
