290a3661b425e49cf8d1aafd7ed65458e737e38c
Robert Ignasiak <tori@robert-i.com>
2014-06-14 17:33:35 +0200

    Test

--- app/ferret.rb
+++ app/ferret.rb
@@ -28,10 +28,10 @@ class Robert
     @index.search_each("q:#{question}") do | id, score |  
       if @index[id][:a]!=nil
         return "#{@index[id][:a]}" 
-      else
-        regexp_value =  /#{@index[id][:q1]}/.match(q)[1]
-        a = @index[id][:r].gsub(/[*]/, regexp_value)
-        return "#{a}" 
+      # else
+      #   regexp_value =  /#{@index[id][:q1]}/.match(q)[1]
+      #   a = @index[id][:r].gsub(/[*]/, regexp_value)
+      #   return "#{a}" 
       end
     end
     return_messages = Array.new()
@@ -61,11 +61,3 @@ index.search_each('q1|q2|q3|q4:kocham cie') do | id, score |
     end
 end
 =end
-
-#OK <srai> - pozwala definiować synonimy dla pewnych określonych wyrażeń, 
-#OK <random> - pozwala wybrać programowi losowy element z listy możliwych; elementy te definiuje się pomiędzy znacznikami <li> i </li>,
-#OK <that> - pozwala sprawdzić jedną z poprzednią wypowiedź chatterbota, używane do łączenia odpowiedzi w zależności od ostatniego zdania, // działa
-# <think> - używane do przeprowadzenia czynności, które nie mają być wyświetlone dla użytkownika (np. zmiana wartości zmiennej),
-# < get/ > i < set > służą do zapisywania i odczytywania zmiennych,
-# <condition> - określa określać reakcję w zależności od stanu zmiennej,
-# <topic> - pozwala botowi zawęzić pole wyszukiwań szablonów do zadanego działu.
